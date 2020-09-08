3df110125f1003b608cb4c171344270a5ad96b9a
ab.reza <a.re.morteza@gmail.com>
2020-09-08 05:54:35 +0430

    fix navbar

--- src/components/GameNav/GameNav.js
+++ src/components/GameNav/GameNav.js
@@ -5,8 +5,14 @@ export default function GameNav() {
   return (
     <div className="game-nav">
       <div className="game-nav-container">
-        <div>
+        <div className="alarmIcon">
           <Icon name="alarm" />
+        </div>
+        <div className="rastakhiz">
+          <img src={process.env.PUBLIC_URL + '/images/rastakhiz.png'} />
+        </div>
+        <div className="bagpack">
+          <img src={process.env.PUBLIC_URL + '/images/bagpack.png'} />
         </div>
       </div>
     </div>
